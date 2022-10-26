2443aaf78140d569b5513c4e02b731c15e105e17
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-26 08:48:47 -0500

    update font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontfamily: {
+        inter: ['Inter', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
