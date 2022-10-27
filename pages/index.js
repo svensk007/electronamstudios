6a186dd681d96c565adb510d065e35dc292cc8a1
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-27 10:16:44 -0500

    starting index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,19 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div lang="en-US">
       <Head>
         <title>El3ctroNam Studios</title>
         <meta name="Portfolio for El3troNam Studios" content="Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav></nav>
+      <main className="font-Inter bg-black">
+        <section>
+          <nav>
+            <h1>Title</h1>
+          </nav>
+        </section>
+      </main>
     </div>
   );
 }
