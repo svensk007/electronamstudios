1e0c03cdc92ceee6f58f7939f31558cc28bfde76
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-27 10:01:34 -0500

    update next.config.js image loader

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
-
+module.exports = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+}
 module.exports = nextConfig;
