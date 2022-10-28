9f0c846f008a68666acb45a99387373a6cadfde0
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-28 13:32:52 -0500

    trying to fix styling

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    loader: 'akamai',
-    path: '',
+    loader: "akamai",
+    path: "",
   },
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme")
-
 module.exports = {
-  important: true,
   content: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
-      animation: {
-        text: "text 3s ease infinite",
-      },
-      keyframes: {
-        text: {
-          "0%, 100%": {
-            "background-size": "200% 200%",
-            "background-position": "left center",
-          },
-          "50%": {
-            "background-size": "200% 200%",
-            "background-position": "right center",
-          },
-        },
+      Inter: ["Inter", "sans-serif"],
+    },
+      colors: {
+        'hot-pink' : "#fa00ff",
+        'regal-blue' : "#00a3ff",
       },
     },
   },
-  plugins: [
-    "postcss-import",
-    "tailwindcss",
-    "autoprefixer"
-  ],
-};
+  plugins: [],
+}
