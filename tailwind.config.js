1dd88c527c8945bfbb29f2cac6e4c0aa5c93910d
svensk007 <83252177+svensk007@users.noreply.github.com>
2022-10-28 11:14:26 -0500

    styling onnce again i think

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
